b51331a3fff7cc27f7521a568c9a71c85281bcf1
Brendan Devenney <brendan@devenney.io>
2016-02-06 23:24:36 +0000

    Migrate to Gulp static site generation with basic optimisation.

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+/*jslint node: true*/
+
+"use strict";
+
+var gulp = require('gulp');
+
+var cssmin = require('gulp-cssmin');
+var concat = require('gulp-concat');
+var critical = require('critical').stream;
+var eol = require('gulp-eol');
+var fileinclude = require('gulp-file-include');
+var markdown = require('markdown');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+gulp.task('critical', function () {
+    return gulp.src('dist/*.html')
+        .pipe(critical({base: 'dist/', minify: true, inline: true, width: 3840, height: 2160, css: ['dist/css/main.css']}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', function () {
+	return gulp.src(['app/index.html'])
+		.pipe(fileinclude({
+			indent: true,
+			filters: {
+				markdown: markdown.parse
+			}
+		}))
+		.pipe(eol())
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {
+    return gulp.src(['app/css/images/*'])
+        .pipe(gulp.dest('dist/css/images'));
+});
+
+gulp.task('sass', function () {
+	return gulp.src('app/sass/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('css', function() {
+	return gulp.src('app/css/*.css')
+        .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('minifycss', function() {
+    gulp.src('dist/css/*.css')
+        .pipe(cssmin({ compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('fonts', function() {
+	return gulp.src('app/fonts/*')
+		.pipe(gulp.dest('dist/fonts/'));
+});
+
+gulp.task('js-base', function () {
+	return gulp.src(['app/js/base/*.js'])
+		.pipe(concat('core.js'))
+		.pipe(gulp.dest('dist/js'))
+		.pipe(rename('core.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('js-ie', function () {
+	return gulp.src('app/js/ie/*.js')
+		.pipe(concat('ie.js'))
+		.pipe(gulp.dest('dist/js'))
+		.pipe(rename('ie.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('js-lib', function() {
+	return gulp.src(['app/js/lib/*.js'])
+		.pipe(concat('lib.js'))
+                .pipe(gulp.dest('dist/js'))
+                .pipe(rename('lib.min.js'))
+                .pipe(uglify())
+                .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('js-jquery', function() {
+	return gulp.src(['app/js/jquery.min.js', 'app/js/skel.min.js'])
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('scripts', ['js-base', 'js-ie', 'js-jquery', 'js-lib']);
+gulp.task('minify', ['minifycss']);
+
+gulp.task('default', ['css', 'images', 'fonts', 'html', 'sass', 'scripts', 'critical', 'minify']);
