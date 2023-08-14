555ef81c9995c48df2f410f35116f97c17df5c8c
Brendan Devenney <brendan@devenney.io>
2023-08-14 17:40:08 +0100

    :sparkles: Lightswitch magic

--- src/js/direct/switch.js
+++ src/js/direct/switch.js
@@ -1,11 +1,38 @@
-let isLight = true
+const dataThemeAttribute = "data-theme"
+const lightOffText="Cast: Light (Evocation)"
+const lightOnText="Cast: Darkness (Evocation)"
+
+let firstInvocation = true
+
+function isLight() {
+    if (firstInvocation) {
+        html = document.documentElement
+
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            html.setAttribute(dataThemeAttribute, 'dark')
+        } else {
+            html.setAttribute(dataThemeAttribute, 'light')
+        }
+
+        firstInvocation = false
+    }
+
+    dataTheme = html.getAttribute(dataThemeAttribute)
+    return (dataTheme == 'light')
+}
 
 // Toggle theme
 const switchTheme = (event) => {
     event.preventDefault();
 
-    isLight = !isLight
-
     html = document.documentElement
-    html.setAttribute('data-theme', isLight? 'light':'dark')
+    html.setAttribute(dataThemeAttribute, isLight()? 'dark':'light')
+
+    setLightswitchTooltip()
 };
+
+function setLightswitchTooltip() {
+    lightswitch = document.getElementById("lightswitch")
+    
+    lightswitch.setAttribute("data-tooltip", isLight()? lightOnText : lightOffText)
+}
