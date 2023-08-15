90b47c4ca9153249454ffe831ea88185c9a9838e
Brendan Devenney <brendan@devenney.io>
2023-08-15 13:26:20 +0100

    :sparkles: ARG

---
+++ src/js/direct/arg.js
@@ -0,0 +1,51 @@
+let theFuture = "Geological Unmanned Terraforming System";
+
+function initARG() {
+    egg = document.getElementById("modal-easter-egg")
+    egg.addEventListener('mouseup', function() {
+        var text = getSelectedText()
+        if (text == theFuture) {
+            crtSwitch()
+            theWillOfOne()
+        }
+    })
+}
+
+
+function getSelectedText() {
+    if (window.getSelection) {
+        return window.getSelection().toString();
+    } else if (document.selection) {
+        return document.selection.createRange().text;
+    }
+    return '';
+}​
+
+function theWillOfOne() {
+    banner = document.getElementById("upper-nav-middle")
+
+    let img = document.createElement("img")
+    img.setAttribute("src", "images/mm.png")
+    img.setAttribute("height", 32)
+    img.setAttribute("width", 32)
+
+    img.setAttribute("onClick", "protomanWasBorn()")
+
+    banner.append(img)
+}
+
+function protomanWasBorn() {
+    main = document.getElementById("main")
+
+    let iframe = document.createElement("iframe")
+    iframe.setAttribute("src", "https://megaman.pomle.com/")
+    iframe.setAttribute("class", "fullscreen")
+    iframe.setAttribute("scrolling", "no")
+    iframe.setAttribute("frameborder", 0)
+
+    main.prepend(iframe)
+}
+
+function crtSwitch() {
+    document.body.className += "crt";
+}
--- src/js/direct/init.js
+++ src/js/direct/init.js
@@ -1,3 +1,4 @@
 function init() {
     setLightswitchTooltip()
+    initARG()
 }
