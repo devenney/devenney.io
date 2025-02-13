e29c4dd5f0bfb48dc958a6faadfc5a9fe8c777e4
Brendan Devenney <brendan@devenney.io>
2025-02-13 22:21:36 +0000

    ✨ 2025 Refactor (#104)
    * :sparkles: 2025 refactor and blog!
    
    Signed-off-by: Brendan Devenney <brendan@devenney.io>
    
    * :sparkles: fix blog content and rendering
    
    Signed-off-by: Brendan Devenney <brendan@devenney.io>
    
    * :wrench: fix dependencies
    
    Signed-off-by: Brendan Devenney <brendan@devenney.io>
    
    ---------
    
    Signed-off-by: Brendan Devenney <brendan@devenney.io>

--- src/js/direct/init.js
+++ src/js/direct/init.js
@@ -1,4 +1,15 @@
 function init() {
     setLightswitchTooltip()
     initARG()
+    initTheme()
+}
+
+function initTheme() {
+    const prismLink = document.getElementById("prism-theme");
+
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        prismLink.setAttribute("href", "/css/prism-tomorrow.css")
+    } else {
+        prismLink.setAttribute("href", "/css/prism.css");
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,58 @@
 const path = require('path');
+const fs = require("fs-extra");
+const glob = require("glob");
+const frontMatter = require("front-matter");
+
+const { format, parseISO } = require("date-fns");
+const { enGB } = require("date-fns/locale");
 
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const MarkdownIt = require("markdown-it");
+const md = new MarkdownIt();
+const BLOG_DIR = path.resolve(__dirname, "src/content/blog");
+
+const handlebarsLoaderOptions = {
+  helperDirs: [path.resolve(__dirname, "src/js/handlebars-helpers")],
+  precompileOptions: {
+    knownHelpersOnly: true,
+  }
+};
+
+const blogPosts = glob.sync(`${BLOG_DIR}/*.md`).map((file) => {
+  const content = fs.readFileSync(file, "utf-8");
+  const { attributes, body } = frontMatter(content);
+
+  // Ensure a valid date and format it before passing to Handlebars
+  let formattedDate = "Unknown Date";
+  if (attributes.date) {
+      try {
+          const parsedDate = parseISO(attributes.date);
+          formattedDate = format(parsedDate, "do MMMM yyyy", { locale: enGB });
+      } catch (err) {
+          console.error(`Error formatting date for ${file}:`, err);
+      }
+  }
+
+  return {
+      title: attributes.title,
+      author: attributes.author || "Brendan Devenney",
+      date: formattedDate,
+      description: attributes.description,
+      slug: path.basename(file, ".md"),
+      content: md.render(body),
+  };
+});
+
+const blogPages = blogPosts.map((post) => {
+  return new HtmlWebpackPlugin({
+      filename: `blog/${post.slug}.html`,
+      template: "src/templates/blog-post.hbs",
+      inject: false,
+      templateParameters: post,
+  });
+});
 
 module.exports = {
   entry: "./src/index.js",
@@ -14,9 +65,7 @@ module.exports = {
       {
         test: /\.hbs$/,
         loader: "handlebars-loader",
-        options: {
-          helperDirs: path.resolve(__dirname, "./src/js/handlebars-helpers")
-        }
+        options: handlebarsLoaderOptions,
       },
       {
         test: /\.scss$/,
@@ -31,19 +80,37 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
           {
-            from: "src/images",
-            to: "images",
+            from: "src/assets",
+            to: "assets",
           },
           {
             from: "src/js/direct",
             to: "js",
           },
+          {
+            from: "node_modules/prismjs/themes/prism.css",
+            to: "css/prism.css"
+          },
+          {
+            from: "node_modules/prismjs/themes/prism-tomorrow.css",
+            to: "css/prism-tomorrow.css"
+          },
         ]
       }),
     new HtmlWebpackPlugin({
-      hash: true,
-      template: '!!handlebars-loader?helperDirs=' + path.resolve(__dirname, 'src/js/handlebars-helpers') + '!src/templates/index.hbs'
-    })
+        hash: true,
+        template: "src/templates/index.hbs",  // Remove query string loader
+        templateParameters: {},
+    }),
+    ...blogPages, // Generates blog post pages dynamically
+    new HtmlWebpackPlugin({
+      filename: "blog/index.html",
+      template: "src/templates/blog-index.hbs",
+      inject: false,
+      templateParameters: {
+        blogPosts: blogPosts,
+      },
+    }),
   ],
   resolve: {
     alias: {
