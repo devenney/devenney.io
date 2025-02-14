be232465e2b391c4d3bd6cb181c2bc83296427b1
Brendan Devenney <brendan@devenney.io>
2025-02-14 19:18:05 +0000

    :wrench: handle theme null
    Signed-off-by: Brendan Devenney <brendan@devenney.io>

--- src/js/direct/init.js
+++ src/js/direct/init.js
@@ -5,15 +5,14 @@ function init() {
 
 function initTheme() {
     theme = localStorage.getItem("theme")
-    if (theme == "auto") {
+    if (theme == "auto" || theme == null) {
         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
             theme = "dark"
 
         } else {
             theme = "light"
         }
-        localStorage.setItem("theme", theme)
-    } 
+    }
 
     setTheme(theme);
 }
